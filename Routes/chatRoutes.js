bc9357808aba9a45e5c40236414b92e9e7c62207
Tushar Malhotra <malhotratushar999@gmail.com>
2023-09-19 21:27:51 +0530

    fetchgroups

--- Routes/chatRoutes.js
+++ Routes/chatRoutes.js
@@ -5,7 +5,7 @@ const { accessChat,fetchChat, createGroupChat, fetchGroups, groupExit } = requir
 const router = express.Router()
 router.route("/").post(protect,accessChat)
 router.route("/").get(protect, fetchChat)
-router.route("/fetchGroups").get(protect, fetchGroups);
+router.route("/fetchGroups").post(protect,fetchGroups);
 router.route("/createGroup").post(protect, createGroupChat);
 router.route("/groupExit").put(protect, groupExit);
 
